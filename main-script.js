1763ae466532884cf356f0190287d37cf44bc8a2
C4ptainHook <boykostudyinkpi@gmail.com>
2023-11-27 13:32:34 +0200

    2/5 tasks finished

--- main-script.js
+++ main-script.js
@@ -7,3 +7,19 @@ const div4Content = div4.innerHTML;
 div4.innerHTML = div5.innerHTML;
 div5.innerHTML = div4Content;
 }
+
+function calculateArea(event) {
+    event.preventDefault();
+
+    const sideA = parseFloat(document.getElementById('sideA').value);
+    const sideB = parseFloat(document.getElementById('sideB').value);
+    const sideC = parseFloat(document.getElementById('sideC').value);
+
+    if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA) {
+        const s = (sideA + sideB + sideC) / 2;
+        const area = Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+        document.getElementById('result-field').innerText = `Площа: ${area}`;
+    } else {
+        alert('Сума двох сторін перевищує значення 3-ї!');
+    }
+}
